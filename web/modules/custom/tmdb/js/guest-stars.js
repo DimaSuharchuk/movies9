380b16a5facb11a279a1c2175fb28057cb4c1a97
Senya <suharchuk.dima@gmail.com>
2020-06-02 17:29:01 +0300

    Refactored guest stars accordion used by Vanilla JS

--- web/modules/custom/tmdb/js/guest-stars.js
+++ web/modules/custom/tmdb/js/guest-stars.js
@@ -1,10 +1,79 @@
-(($, Drupal) => {
+(Drupal => {
   Drupal.behaviors.guestStarsAccordion = {
-    attach: context => {
-      $(".guest-stars-label", context).on("click", function () {
-        $(this).siblings('.guest-stars-content').slideToggle();
+    attach: () => {
+      document.querySelectorAll(".guest-stars-label").forEach(star => {
+        star.addEventListener("click", function () {
+          slideToggle(star.nextElementSibling, 500);
+        });
       });
     }
   };
 
-})(jQuery, Drupal);
+  const slideUp = (target, duration = 500) => {
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + "ms";
+    target.style.boxSizing = "border-box";
+    target.style.height = target.offsetHeight + "px";
+    target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+      target.style.display = "none";
+      target.style.removeProperty("height");
+      target.style.removeProperty("padding-top");
+      target.style.removeProperty("padding-bottom");
+      target.style.removeProperty("margin-top");
+      target.style.removeProperty("margin-bottom");
+      target.style.removeProperty("overflow");
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
+    }, duration);
+  }
+
+  const slideDown = (target, duration = 500) => {
+    target.style.removeProperty("display");
+    let display = window.getComputedStyle(target).display;
+
+    if (display === "none") {
+      display = "flex";
+    }
+
+    target.style.display = display;
+    const height = target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.boxSizing = "border-box";
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + "ms";
+    target.style.height = height + "px";
+    target.style.removeProperty("padding-top");
+    target.style.removeProperty("padding-bottom");
+    target.style.removeProperty("margin-top");
+    target.style.removeProperty("margin-bottom");
+    window.setTimeout(() => {
+      target.style.removeProperty("height");
+      target.style.removeProperty("overflow");
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
+    }, duration);
+  }
+
+  const slideToggle = (target, duration = 500) => {
+    if (window.getComputedStyle(target).display === "none") {
+      return slideDown(target, duration);
+    }
+    else {
+      return slideUp(target, duration);
+    }
+  }
+
+})(Drupal);

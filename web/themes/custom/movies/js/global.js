6c92bf496afdfa8c4e8c74666e959063ce37edfd
Senya <suharchuk.dima@gmail.com>
2020-11-23 05:17:52 +0200

    Added dark theme. Refactored styles

--- web/themes/custom/movies/js/global.js
+++ web/themes/custom/movies/js/global.js
@@ -1,9 +1,8 @@
-"use strict";
-
 (D => {
+  "use strict";
+
   D.behaviors.languageSwitcher = {
     attach: (context, settings) => {
-      settings.menuOnce = settings.menuOnce || false;
       const trigger = document.querySelector(".desktop-nav .active-language");
       if (trigger && !settings.menuOnce) {
         settings.menuOnce = true;
@@ -23,6 +22,35 @@
           this.nextElementSibling.classList.toggle("open");
         });
       }
+    },
+  };
+
+  D.behaviors.themeSwitcher = {
+    attach: (context, settings) => {
+      if (!settings.themeSwitcherOnce) {
+        settings.themeSwitcherOnce = true;
+
+        document.querySelectorAll(".theme-switcher").forEach(wrapper => {
+          wrapper.addEventListener("click", () => {
+            if (Cookies.get("theme-mode") !== "dark") {
+              Drupal.behaviors.themeSwitcher.setDark(true);
+            }
+            else {
+              Drupal.behaviors.themeSwitcher.setLight();
+            }
+          });
+        });
+      }
+    },
+    setDark: (updateCookie) => {
+      document.body.dataset.theme = "dark";
+      if (updateCookie) {
+        Cookies.set("theme-mode", "dark", {expires: 365});
+      }
+    },
+    setLight: () => {
+      Cookies.set("theme-mode", "");
+      document.body.dataset.theme = "";
     },
   };
 

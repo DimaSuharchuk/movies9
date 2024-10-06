2544cea2119ff8b5114f5d8ff4474b949f0fcbf6
DimaSuharchuk <suharchuk.dima@gmail.com>
2024-10-06 03:41:59 +0300

    Added more info about IPs to module "country_access_filter" settings form

--- web/modules/custom/country_access_filter/src/Form/CountryAccessFilterSettingsForm.php
+++ web/modules/custom/country_access_filter/src/Form/CountryAccessFilterSettingsForm.php
@@ -2,10 +2,25 @@
 
 namespace Drupal\country_access_filter\Form;
 
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CountryAccessFilterSettingsForm extends ConfigFormBase {
+
+  protected Connection $db;
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+
+    $instance->db = $container->get('database');
+
+    return $instance;
+  }
 
   protected function getEditableConfigNames(): array {
     return ['country_access_filter.settings'];
@@ -39,6 +54,70 @@ class CountryAccessFilterSettingsForm extends ConfigFormBase {
       '#type' => 'checkbox',
       '#title' => $this->t('Debug mode'),
       '#default_value' => $config->get('debug_mode'),
+    ];
+
+    // IPs info.
+    $query = $this->db
+      ->select('country_access_filter_ips', 'i')
+      ->fields('i', ['status'])
+      ->groupBy('status');
+    $query->addExpression('COUNT(status)');
+    $counts = $query->execute()->fetchAllKeyed();
+
+    $count_allowed = $counts[1] ?? 0;
+    $count_denied = $counts[0] ?? 0;
+    $count_all = $count_allowed + $count_denied;
+
+    $form['info'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Info'),
+      '#open' => TRUE,
+    ];
+    $form['info']['ips_all'] = [
+      '#type' => 'item',
+      '#title' => $this->t('IPs count all'),
+      '#markup' => $count_all,
+    ];
+    $form['info']['ips_allowed'] = [
+      '#type' => 'item',
+      '#title' => $this->t('IPs count allowed'),
+      '#markup' => $count_allowed,
+    ];
+    $form['info']['ips_denied'] = [
+      '#type' => 'item',
+      '#title' => $this->t('IPs count denied'),
+      '#markup' => $count_denied,
+    ];
+
+    // Countries.
+    $query = $this->db
+      ->select('country_access_filter_ips', 'i')
+      ->fields('i', ['country_code'])
+      ->groupBy('country_code');
+    $query->addExpression('COUNT(country_code)');
+    $countries = $query->execute()->fetchAllKeyed();
+
+    $header = [
+      'country' => $this->t('Country'),
+      'count' => $this->t('IPs count'),
+    ];
+    $rows = [];
+
+    foreach ($countries as $country => $count) {
+      $rows[] = [
+        'country' => $country,
+        'count' => $count,
+      ];
+    }
+
+    $form['info']['countries'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Countries'),
+    ];
+    $form['info']['countries']['table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
     ];
 
     return parent::buildForm($form, $form_state);
